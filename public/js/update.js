56df14d06bbc8167e1376dcb3e73aec4946c2331
Age <ykjage@gmail.com>
2018-01-18 01:12:37 +0800

    v1.0 乞丐版

--- public/js/update.js
+++ public/js/update.js
@@ -1,48 +1,102 @@
-$(document).ready(function(){
-
-//     var fd = new FormData(),
-//         xhr = new XMLHttpRequest(),
-//         input;
-
-
-//  	$("#btn").click(function(){
-//  		create_input();
-//  		console.log('s');
-//   	});
+$(document).ready(function(){        
+        //进度条
+        $('#file_example .ui.progress')
+          .progress({
+            duration : 100,
+            total    : 100
+        });
 
 
-//   	function create_input(){
-// 		input = document.createElement('input');
-//         input.setAttribute('id', 'myUploadInput');
-//         input.setAttribute('type', 'file');
-//         input.setAttribute('name', 'file');
-//         input.setAttribute('multiple', true);
-//         $("body").append(input);
-//         input.style.display = 'none';
-//         input.click();
+        //登录
+        $('.menu .right.item').click(function () {
+            $('.ui.modal')
+                .modal('show')
+            ;
+        });
 
-//         input.onchange = function() {
-// 	    	if(input.files.length == 0) { return false; }
-// 	        for(var i = 0; i < input.files.length; i++) {
-// 	        	var file = input.files[i];
-// 	        	var type = file.name.split('.').pop();
-// 	            fd.append('file' + i, file);
-// 	        }
-//         }
-//   	}
+        $('#login-button').click(function () {
+            var user = $("#user").val();
+            console.log('hi there :' + user);
+            $('.ui.modal')
+                .modal('hide')
+            ;
+            $('.menu .right.item').text(user);
+        });
 
 
+        //返回首页
+        $('.reply.icon').click(function () {
+            $('.ui.cards').empty();
+            console.log('return');
+            $('#path').text('/');
+            $.get('listdir',function (res) {
+                var dirs = res.dirs;
+                dirs.forEach(function (value) {
+                    append_dir(value.file_dir);
+                });
+                $('.path-enter').click(function () {
+                    console.log(1);
+                    var dir = $(this).parents('.card').attr("id");
+                    console.log(dir);
+                    $('#path').text('/'+dir);
+                    $('.ui.cards').empty();
+                    $.get('listfile',{dir:dir},function (res) {
+                        var files = res.files;
+                        // console.log("res.files"+res);
+                        files.forEach(function (file) {
+                            file.file_Name = file.fileName;
+                            file.file_url = file.URL;
+                            file.file_time = file.file_upTime;
+                            append_file(file);
+                        });
+                    });
+                });
+            });
+        });
 
-// });
+        //点击进入文件夹
 
-/*global Qiniu */
-/*global plupload */
-/*global FileProgress */
-/*global hljs */
 
-            note = document.getElementById("note"),
-            prs = document.getElementById("process");
+        function  append_dir(file_dir) {
+            // $('.ui.cards').empty();
+            var elem = "<div class=\'ui card type-directory\' id="+file_dir+"><div class=\'content\'><a class='”header“'>"+file_dir+"</a></div><div class='extra content'><button class='path-enter' ><span class='right floated'><i class='sign in icon'></i>进入</span></button></div></div>";
+            $('.ui.cards').append(elem);
 
+        }
+        function append_file(file) {
+            var elem = "<div class=\"ui card type-file\" ><div class=\"content\"><a href=\"\" class=\"”header“\">file.file_dir</a><div class=\"meta\"><span>file.file_time</span></div><div class=\"description\">"+file.file_Name+"</div></div><div class=\"extra content\"><a href="+file.file_url+"><span class=\"right floated\"><i class=\"download icon\"></i>下载</span></a></div></div>";
+            $('.ui.cards').append(elem);
+        }
+
+        //首页进入
+        $.get('listdir',function (res) {
+            var dirs = res.dirs;
+            dirs.forEach(function (value) {
+                append_dir(value.file_dir);
+            });
+
+            $('.path-enter').click(function () {
+                console.log(1);
+                var dir = $(this).parents('.card').attr("id");
+                console.log(dir);
+                $('#path').text('/'+dir);
+                $('.ui.cards').empty();
+                $.get('listfile',{dir:dir},function (res) {
+                    var files = res.files;
+                    // console.log("res.files"+res);
+                    files.forEach(function (file) {
+                        file.file_Name = file.fileName;
+                        file.file_url = file.URL;
+                        file.file_time = file.file_upTime;
+                        append_file(file);
+                    });
+                });
+            });
+        });
+
+        //文件上传
+        var input;
+        var path;
 
 	    $("#pickfiles").click(function(){
             myUpload({
@@ -50,29 +104,31 @@ $(document).ready(function(){
                 maxSize: 10,
                 multiple: true,
                 beforeSend: function(file) {
-                    note.innerText = "开始上传...";
+                    $('#file_example .label').text("上传中");
                 },
                 callback: function(res, input) {
                     res = JSON.parse(res);
                     if(res.code == 1) {
-                        note.innerText = "上传成功!"
+                        $('#file_example .label').text("上传成功!");
+                        $('#file_example .ui.progress').addClass('success');
+
                     } else {
-                    	note.innerText = res.msg;
+                        $('#file_example .label').text("上传失败!");
+                        $('#file_example .ui.progress').addClass('error'); 
                     }
                     document.body.removeChild(input);
                 },
                 uploading: function(pre) {
-                    prs.innerText = "当前上传进度为：" + pre + "%";
-                }
+                        $('#file_example .ui.progress').progress({
+                          percent: pre
+                        });
+                    }
             });
         })
 
-
- 
         function myUpload(option) {
             var fd = new FormData(),
                 xhr = new XMLHttpRequest(),
-                input;
             input = document.createElement('input');
             input.setAttribute('id', 'myUploadInput');
             input.setAttribute('type', 'file');
@@ -83,23 +139,11 @@ $(document).ready(function(){
             document.body.appendChild(input);
             input.style.display = 'none';
             input.click();
-            var fileType = ['jpg','png'];
             input.onchange = function() {
             	if(input.files.length == 0) { return false; }
                 for(var i = 0; i < input.files.length; i++) {
                 	var file = input.files[i];
                 	var type = file.name.split('.').pop();
-                 //    if(option.maxSize &&  file.size > option.maxSize * 1024 * 1024){
-                 //        alert('请上传小于' + option.maxSize + 'M的文件');
-                 //        document.body.removeChild(input);
-                 //        return false;
-                 //    }
-                 //    if(fileType.indexOf(type.toLocaleLowerCase()) == -1) {
-	                //     alert("暂不支持该类型的文件，请重新选择!");
-	                //     document.body.removeChild(input);
-	                //     return false;
-	                // }
-	                // console.log(fd)
                     fd.append('file' + i, file);
                 }
                 if(option.beforeSend instanceof Function) {
@@ -117,9 +161,7 @@ $(document).ready(function(){
                             }
                         }
                     } else {
-                    	document.body.removeChild(input);
-                        console.log("上传失败！");
-
+                        $('#file_example .label').text("上传失败!");
                     }
                 }
                 xhr.upload.onprogress = function(event) {
@@ -133,22 +175,6 @@ $(document).ready(function(){
         }
 
 
-	$.get("listfile",function(res){
-		var items  = res.items;
-		console.log(items);
-
-		prefix = '/look';
-
-		items.forEach(function(item) {
-          	// console.log(item.key);
-          	var file_name = item.key.slice(prefix.length);
-          	var timestamp = item.putTime;
-          	timestamp = timestamp.toString().slice(0,10);
-          	console.log(timestamp);
-          	var file_time = new Date(timestamp*1000).toLocaleString();
-			$('#concrete-files').append(' <div class="ui card"><div class="content"><a href="" class="”header“">'+prefix+'</a><div class="meta"><span>'+file_time+'</span></div><div class="description">'+file_name+'</div></div>');
-		})
-	})
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,70 @@
 var express = require('express');
 var router = express.Router();
-
+var file_des="1";// 文件描述
+var file_user="1";//文件上传用户
+var url="http://p2h9efhxp.bkt.clouddn.com/";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'Express' });
+    res.render('home.html');
 });
 
 const multiparty = require('connect-multiparty');
 const path = require('path');
 let uploadDir = path.resolve(__dirname, '../temp');
 let multipartMiddleware = multiparty({uploadDir});
- 
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : '115.159.202.238',
+    user     : 'file_root',
+    password : '123456',
+    database : 'file-up'
+});
+connection.connect();
+//  var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '123456',
+//   database : 'file'
+// });
+router.get('/listdir',function(req, res, next){
+    var  sql = 'SELECT distinct file_dir FROM file_information';
+//查
+    connection.query(sql,function (err, result) {
+        if(err){
+            console.log('[SELECT ERROR] - ',err.message);
+            return;
+        }
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');
+        res.json({dirs:result});
+    });
+
+
+
+})
 
 router.get('/listfile',function(req, res, next){
-
-    var accessKey = 'Zvf1stktGElAF_n4TKRduaPoTPfm4rt2WzTaR49K';
-    var secretKey = 'jPXP-8AAayQBFL-KXUQibb_mkdjVCQebQcrc0Wfp';
-    var qiniu = require("qiniu");
-
-    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-    var config = new qiniu.conf.Config();
-    //config.useHttpsDomain = true;
-    config.zone = qiniu.zone.Zone_z0;
-    var bucketManager = new qiniu.rs.BucketManager(mac, config);
-    var bucket = "file-update-tool";
-
-    var bucket_url = 'http://p2h9efhxp.bkt.clouddn.com/';
-    var options = {
-      limit: 10,
-      prefix: 'look/',
-    };
-
-    // bucketManager.stat(bucket, key, function(err, respBody, respInfo) {
-    //     console.log('开始文件');
-    //   if (err) {
-    //     console.log(err);
-    //     //throw err;
-    //   } else {
-    //     if (respInfo.statusCode == 200) {
-    //       console.log(respBody.hash);
-    //       console.log(respBody.fsize);
-    //       console.log(respBody.mimeType);
-    //       console.log(respBody.putTime);
-    //       console.log(respBody.type);
-    //     } else {
-    //       console.log(respInfo.statusCode);
-    //       console.log(respBody.error);
-    //     }
-    //   }
-    // });
-
-    bucketManager.listPrefix(bucket, options, function(err, respBody, respInfo) {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-      if (respInfo.statusCode == 200) {
-        console.log('有');
-        //如果这个nextMarker不为空，那么还有未列举完毕的文件列表，下次调用listPrefix的时候，
-        //指定options里面的marker为这个值
-        var nextMarker = respBody.marker;
-        var commonPrefixes = respBody.commonPrefixes;
-        console.log(nextMarker);
-        console.log(commonPrefixes);
-        var items = respBody.items;
-        items.forEach(function(item) {
-          console.log(item.key);
-          item.url = bucket_url + item.key;
-          console.log(item.putTime);
-          // console.log(item.hash);
-          // console.log(item.fsize);
-          // console.log(item.mimeType);
-          // console.log(item.endUser);
-          // console.log(item.type);
+    var pretix = req.query.dir;
+    // console.log(pretix);
+    var  sql = 'SELECT * FROM file_information where file_dir = ?';
+//查
+    connection.query(sql,pretix,function (err, result) {
+        if(err){
+            console.log('[SELECT ERROR] - ',err.message);
+            return;
+        }
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        console.log('------------------------------------------------------------\n\n');
+        // res.json({dirs:result});
+        result.forEach(function(item) {
+            item.URL=url+item.fileID
         });
 
-        res.json({items:items});
-        // res.json({ titl: 'Exps' });
-      } else {
-        console.log(respInfo.statusCode);
-        console.log(respBody);
-      }
-
+        console.log(result);
+        res.json({files:result});
     });
 
 })
@@ -90,29 +73,28 @@ router.post('/upload', multipartMiddleware, function(req, res, next) {
 
     console.log('文件'+req.files);
     const qiniu = require("qiniu");
- 
+
     //需要填写你的 Access Key 和 Secret Key
     accessKey = 'Zvf1stktGElAF_n4TKRduaPoTPfm4rt2WzTaR49K';
     secretKey = 'jPXP-8AAayQBFL-KXUQibb_mkdjVCQebQcrc0Wfp';
- 
+
     var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
     //要上传的空间
     bucket = 'file-update-tool';
-  
+
     //构建上传策略函数
     function uptoken(bucket, key) {
         // var prefix = 'look/';
         // key = prefix + key;
         // console.log(key);
         var options = {
-          scope: bucket + ":" + key
+            scope: bucket + ":" + key
         }
         let putPolicy = new qiniu.rs.PutPolicy(options);
         return putPolicy.uploadToken(mac);
     }
-  
     //构造上传函数
-    function uploadFile(uptoken, key, localFile, resolve, reject) {
+    function uploadFile(uptoken, key, localFile, resolve, reject,index) {
         var config = new qiniu.conf.Config();
         // 空间对应的机房
         config.zone = qiniu.zone.Zone_z0;
@@ -122,31 +104,59 @@ router.post('/upload', multipartMiddleware, function(req, res, next) {
             if(!err) {
                 // 上传成功， 处理返回值
                 resolve(localFile);
+                var file_name=req.files[index].originalFilename;
+                var date = new Date();
+                var year = date.getFullYear();
+                var month = date.getMonth()+1;
+                var day = date.getDate();
+                var hour = date.getHours();
+                var minute = date.getMinutes();
+                var second = date.getSeconds();
+                // console.log(year+'年'+month+'月'+day+'日 '+hour+':'+minute+':'+second);
+                var file_time=year+'年'+month+'月'+day+'日 '+hour+':'+minute+':'+second;
+                console.log(file_time);
+                console.log("file_name"+file_name);
+                // connection.connect();
+                var prefix='look';
+                var  addsql = 'INSERT INTO file_information(fileID,fileName,fileDes,fileUser,file_dir,file_upTime) VALUES(?,?,?,?,?,?)';
+                //查
+                var addparams=[key,file_name,file_des,file_user,prefix,file_time]
+                connection.query(addsql,addparams,function (err, result) {
+                    if(err){
+                        console.log('[INSERT ERROR] - ',err.message);
+                        return;
+                    }
+                    console.log('--------------------------INSERT----------------------------');
+                    console.log('INSERT ID:',result);
+                    console.log('-----------------------------------------------------------------\n\n');
+                });
+
             } else {
                 // 上传失败， 处理返回代码
                 reject(err);
             }
         });
     }
- 
+
     // 构造Promise数组
     let promiseArr = [];
     for (index in req.files) {
         let p = new Promise((resolve, reject) => {
-              var prefix = 'look/';
+            var prefix = 'look/';
 
-            //上传到七牛后保存的文件名
-            key = req.files[index].originalFilename;
-             key = prefix + key;
-            //生成上传 Token
-            token = uptoken(bucket, key);
-      
-            //要上传文件的本地路径
-            filePath = req.files[index].path;
-     
-            //调用uploadFile上传
-            uploadFile(token, key, filePath, resolve, reject);
-        })
+        //上传到七牛后保存的文件名
+
+        key = Math.ceil(Math.random()*1000);
+        // key = prefix + key;
+        //生成上传 Token
+        token = uptoken(bucket, key);
+
+        //要上传文件的本地路径
+        filePath = req.files[index].path;
+
+        //调用uploadFile上传
+        uploadFile(token, key, filePath, resolve, reject,index);
+    })
 
         promiseArr.push(p);
     }
@@ -155,8 +165,8 @@ router.post('/upload', multipartMiddleware, function(req, res, next) {
     let pAll = Promise.all(promiseArr);
     pAll.then((localFile) => {
         console.log(localFile);
-        res.json({code: 1})
-    }, (err) => {
+    res.json({code: 1})
+}, (err) => {
         console.log(err);
         res.json({code: 0, msg: '上传失败'});
     })
